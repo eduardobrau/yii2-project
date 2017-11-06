8af5ed18ec2a29ee999897ec6b8385510fa35adc
Esteves <esteves@edusites.com.br>
2017-11-06 17:09:24 -0200

    Após criar os models, controllers e views com a ferramenta gii criei o model para o formulário de cadastro CadastroAnuncioForm.php para validação dos dados

---
+++ models/CadastroAnuncioForm.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eduardo
+ * Date: 06/11/17
+ * Time: 11:49
+ */
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class CadastroAnuncioForm extends Model
+{
+  //Campos já existentes na tabela anuncios
+  public $titulo;
+  public $slogan;
+  public $texto;
+  public $telefone;
+  public $endereco;
+  public $site;
+  public $bairro_id;
+  public $categoria_id;
+
+
+
+  public function rules(){
+    return [
+      [['titulo', 'texto', 'telefone', 'endereco', 'bairro_id', 'categoria_id'], 'required'],
+      [['titulo', 'slogan'], 'string', 'max'=>200],
+      [['texto'], 'string'],
+      [['site'], 'string', 'max'=>45],
+      [['bairro_id', 'categoria_id'], 'integer'],
+    ];
+  }
+
+  public function attributeLabels(){
+    return [
+      'id_anuncio' => 'Anuncio',
+      'titulo' => 'Titulo',
+      'slogan' => 'Slogan',
+      'texto' => 'Texto',
+      'telefone' => 'Telefone',
+      'endereco' => 'Endereço',
+      'site' => 'Site',
+      'bairro_id' => 'Bairro',
+      'categoria_id' => 'Categoria',
+    ];
+  }
+}
