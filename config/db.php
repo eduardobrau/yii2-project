05d6de9790b58fed7e9d408beccab609e83283fc
Esteves <esteves@edusites.com.br>
2017-11-13 18:39:19 -0200

    Criado banco de dados yii2 e alterado os parametros de config em db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost:3306;dbname=yii',
+    'dsn' => 'mysql:host=localhost:3306;dbname=yii2',
     'username' => 'root',
     'password' => '12345',
     'charset' => 'utf8',
