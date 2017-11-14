539eb1c48c9efcb832ef4a1e54ae1866a1db182d
Esteves <esteves@edusites.com.br>
2017-11-14 17:30:44 -0200

    Adicionado propriedade template e buttons para view, update, delete

--- views/anuncios/index.php
+++ views/anuncios/index.php
@@ -1,40 +1,58 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\StringHelper;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\AnunciosSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Anuncios';
+$this->title = 'Anúncios';
 $this->params['breadcrumbs'][] = $this->title;
+
 ?>
+
 <div class="anuncios-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
         <?= Html::a('Create Anuncios', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
+
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
-        'columns' => [
+        'columns' =>
+        [
             ['class' => 'yii\grid\SerialColumn'],
-
             'id',
             'titulo',
             'slogan',
             'texto:ntext',
+            //StringHelper::truncate('texto:ntext', 20),
             'telefone',
-            // 'endereco',
-            // 'site',
-            // 'bairro_id',
-            // 'categoria_id',
-
-            ['class' => 'yii\grid\ActionColumn'],
+            ['class' => 'yii\grid\ActionColumn',
+                'template'=>'{view} {update} {delete}',
+                'buttons'=>
+                [
+                    'view' => function ($url, $model)
+                    {
+                        //echo '<pre>'; var_dump($model); echo '</pre>';
+                        return Html::a('<button class="btn btn-xs glyphicon glyphicon-eye-open"></button>', "/anuncios/{$model->id}?bairro_id={$model->bairro_id}&categoria_id={$model->categoria_id}");
+                    },
+                    'update' => function ($url, $model)
+                    {
+                        return Html::a('<button class="btn btn-xs glyphicon glyphicon-pencil"></button>', "/anuncios/{$model->id}?bairro_id={$model->bairro_id}&categoria_id={$model->categoria_id}");
+                    },
+                    'delete' => function ($url, $model)
+                    {
+                        return Html::a('<button class="btn btn-xs glyphicon glyphicon-trash"></button>', "/anuncios/delete/{$model->id}?bairro_id={$model->bairro_id}&categoria_id={$model->categoria_id}");
+                    }
+                ]
+            ],
         ],
     ]); ?>
+
 </div>
