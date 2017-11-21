cfe40405dd7b24e453c08aa867ae86f82d103cc6
Esteves <esteves@edusites.com.br>
2017-11-21 17:41:37 -0200

    Adicionado main.css, pasta img e view home.php

---
+++ views/site/home.php
@@ -0,0 +1,29 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Congratulations!</h1>
+
+        <p class="lead">You have successfully created your Yii-powered application.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>Escolha a Cidade</h2>
+
+                <img src="">
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+        </div>
+
+    </div>
+</div>
