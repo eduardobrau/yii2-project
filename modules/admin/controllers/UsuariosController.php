73ef266396f0f4ae9abdb48ec92dc93e3b89afda
Esteves <esteves@edusites.com.br>
2017-12-16 11:56:31 -0200

    Criado o esqueleto de modules e adicionado controller Usuarios e sua view

---
+++ modules/admin/controllers/UsuariosController.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace app\modules\admin\controllers;
+
+use Yii;
+use app\models\DBUsuarios;
+use app\models\DBUsuariosSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use app\models\User;
+
+/**
+ * DBUsuariosController implements the CRUD actions for DBUsuarios model.
+ */
+class UsuariosController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => \yii\filters\AccessControl::className(),
+                'only' => ['index', 'create', 'update'],
+                'rules' => [                  
+                  [
+                    'actions' => ['index', 'create'],
+                    'allow' => true,
+                    'matchCallback' => function ($rule, $action){
+                        if (Yii::$app->user->isGuest)
+                            return false;
+                        elseif (!Yii::$app->user->identity->admin)
+                            return false;
+                        else
+                            return true;
+                    },
+                  ], 
+                  [
+                    'actions' => ['update'],
+                    'allow' => true,
+                    'matchCallback' => function ($rule, $action){
+                        
+                    },
+                  ],                 
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all DBUsuarios models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new DBUsuariosSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single DBUsuarios model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new DBUsuarios model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new DBUsuarios();
+        $user = new User();
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            //echo '<pre>'; print_r( Yii::$app->request->post() ) ; echo '</pre>';die;
+            $model->password = $user->setPassword($model->password);
+            $model->save();
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing DBUsuarios model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing DBUsuarios model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the DBUsuarios model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return DBUsuarios the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = DBUsuarios::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
