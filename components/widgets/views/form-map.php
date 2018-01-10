5554249f5635453ccb890c62747293facc52ef6d
Esteves <esteves@edusites.com.br>
2018-01-10 10:30:34 -0200

    Adicionado propriedade name aos selects

--- components/widgets/views/form-map.php
+++ components/widgets/views/form-map.php
@@ -1,38 +1,39 @@
 
 <div class="jumbotron">
-  <form class="form-inline center-block">
-    <select id="selectTag" class="form-control select-tag">
-      <option>O que procura?</option>
+  <form class="form-inline center-block" action="/anuncios/pesquisar" method="get">
+    <select name="tag_id" id="selectTag" class="form-control select-tag">
+      <option value="">O que procura?</option>
       <?php 
         foreach ($tags as $tag) {
           echo "<option value=\"{$tag->id}\"> {$tag->tag} </option>";
         }
       ?>
     </select>
-    <select id="selectCategory" class="form-control">
-      <option>Em qual categoria?</option>
+    <select name="categoria_id" id="selectCategory" class="form-control">
+      <option value="">Em qual categoria?</option>
       <?php 
         foreach ($categorias as $categoria) {
           echo "<option value=\"{$categoria->id}\"> {$categoria->categoria} </option>";
         }
       ?>
     </select>
-    <select 
+    <select
+      name="cidade_id"
       id="selectCity" 
       class="form-control" 
       data-drop-change="selectBairro"
       data-action-url="/bairros/listar-bairros">
-      <option>Em qual Cidade?</option>
+      <option value="">Em qual Cidade?</option>
       <?php
         foreach($cidades as $cidade) {
           echo "<option value=\"{$cidade->id}\"> {$cidade->cidade} </option>";
         }
       ?>
     </select>
-    <select id="selectBairro" class="form-control selectBairro">
-        <option>Em qual Bairro?</option>
+    <select name="bairro_id" id="selectBairro" class="form-control selectBairro">
+        <option value="">Em qual Bairro?</option>
     </select>
 
-      <button type="submit" class="btn btn-success">Pesquisar</button>
+      <button type="submit" class="btn btn-primary">Pesquisar</button>
   </form>
 </div>
