1df70540cf4db86a0b6ed41ddabc4527b7ea1006
Esteves <esteves@edusites.com.br>
2018-01-16 20:01:19 -0200

    Agora além de criar um novo usuário este controller carrega as propriedades do Model ValidadorUsuarios com uma  key gerada aleartoriamente para ser validada posteriormente pelo usuário quando receber um email com um link apontando para o ControllerValidadorUsuarios e parametro key que vai atualizar o status do usuario para ativo

--- modules/admin/controllers/UsuariosController.php
+++ modules/admin/controllers/UsuariosController.php
@@ -9,6 +9,7 @@ use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use app\models\User;
+use app\models\ValidadorUsuarios;
 
 /**
  * DBUsuariosController implements the CRUD actions for DBUsuarios model.
@@ -89,15 +90,52 @@ class UsuariosController extends Controller
      */
     public function actionCreate()
     {
+        //echo '<pre>'; print_r( Yii::$app->homeUrl ) ; echo '</pre>';die;
         $model = new DBUsuarios();
-        $user = new User();
-
+        
         if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            //echo '<pre>'; print_r( Yii::$app->request->post() ) ; echo '</pre>';die;
-            $model->password = $user->setPassword($model->password);
-            $model->save();
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
+            
+            Yii::$app->session->setFlash('usuarioFormSubmitted');
+
+            $transaction = Yii::$app->db->beginTransaction();
+            
+            try {
+                $user = new User();
+                $model->password = $user->setPassword($model->password);
+                $model->save();
+                
+                $valUsuario = new ValidadorUsuarios();
+                $valUsuario->usuario_id = $model->id; 
+                $strRamdom = Yii::$app->security->generateRandomString();
+                $valUsuario->key = $strRamdom;
+                
+                if( $valUsuario->validate() && $valUsuario->save() ){
+                    
+                    $url = "http://yii2/validador-usuarios/validar?key=".$strRamdom;
+                    $content = "<p>Obrigado por se cadastrar na plataforma de busca ";
+                    $content .= "de comêrcios e profissionais autonômos Encontra ABC para ";
+                    $content .= "ativar sua conta basta clicar no link abaixo e fazer ótimos ";
+                    $content .= "negocios</p> <a href='{$url}'>{$url}</a>";
+                
+                    Yii::$app->mailer->compose('@app/mail/layouts/html',['content'=>$content])
+                    ->setTo($model->email)
+                    ->setFrom(Yii::$app->params['adminEmail'])
+                    ->setSubject('Cadastro Encontra ABC')
+                    ->setHtmlBody($content)
+                    ->send();
+    
+                }
+                
+                $transaction->commit();
+
+            }catch(Exception $e){
+                $transaction->rollBack();
+                throw $e;
+            }
+
+            return $this->refresh();
+
+        }else {
             return $this->render('create', [
                 'model' => $model,
             ]);
