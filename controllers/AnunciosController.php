5737d3953eadd356f69dc8478a836c06d5f0aa53
Esteves <esteves@edusites.com.br>
2018-02-06 20:27:27 -0200

    Adicionado regras ACF onde somente usuário admin pode acessar listar() e index(), também foi deletado os parametros inrelevantes ,  das actions deixando apenas  já que  é um inteiro gerado com 15 digitos pelo BaseActiveRecord, adicionado filtros em todos os model que carrega dados para preenchimento do formulário orderBy(['cidade'=>SORT_ASC]) e atribuido a variaveis mais legiveis cidadesID, categoriasID a fim de identificar as propriedades que só existem para preencher o _form. No create() foi reduzido as variaveis que antes era setada manual do CadastroAnuncioForm para Anuncios para depois ser salvo, agora Anuncios já recebe todos os dados que precisa diretamente da requisição post('CadastroAnuncioForm') reduzindo mais o código então salva os dados na tabela principal e depois salva as relações NxN no foreach. Já no Update a principal novidade foi carregar os dados relacionados por exemplo ->redes_sociais = ->anunciosRedesSociais para ser avaliado no _form e também para ser deletado antes de atualizar os novos valores

--- controllers/AnunciosController.php
+++ controllers/AnunciosController.php
@@ -40,16 +40,32 @@ class AnunciosController extends Controller
       ],
       'access' => [
         'class' => \yii\filters\AccessControl::className(),
-        'only' => ['create', 'update', 'delete'],
+        /* The only option specifies that the Access Control Filter
+        should only be applied to the index, update, create and delete actions.
+        All other actions in the site controller are not subject to the access
+        control. */
+        'only' => ['index','update','create','delete'],
+        /* While a user is requesting to execute an action, ACF will check a list
+        of access rules to determine if the user is allowed to access the
+        requested action. */
         'rules' => [
           // deny all POST requests
-          // [
-          //   'allow' => false,
-          //   'verbs' => ['POST']
-          // ],
+          [
+            'actions' => ['index','listar'],
+            'allow' => true,
+            'matchCallback' => function ($rule, $action){
+              if (Yii::$app->user->isGuest)
+                return false;
+              elseif (Yii::$app->user->identity->admin)
+                return true;
+              else
+                return false;
+            }
+          ],
           // allow authenticated users
           [
             'allow' => true,
+            'actions' => ['update','create','delete'],
             'roles' => ['@'],
           ],
           // everything else is denied
@@ -136,10 +152,10 @@ class AnunciosController extends Controller
    * @param integer $categoria_id
    * @return mixed
   */
-  public function actionView($id, $bairro_id, $categoria_id)
+  public function actionView($id)
   {
     return $this->render('view', [
-      'model' => $this->findModel($id, $bairro_id, $categoria_id),
+      'model' => $this->findModel($id),
     ]);
   }
 
@@ -154,45 +170,40 @@ class AnunciosController extends Controller
 
     $form = new CadastroAnuncioForm();
     // Carregar todas as cidades para atribuir a propriedade $id_cidade no _form
-    $form->cidade_id = Cidades::find()->all();
-    $form->categoria_id = Categorias::find()->all();
-    $form->redesSociaisID = RedesSociais::find()->all();
-    $form->tagsID = Tags::find()->all();
+    $form->cidadesID = Cidades::find()->orderBy(['cidade'=>SORT_ASC])->all();
+    $form->categoriasID = Categorias::find()->orderBy(['categoria'=>SORT_ASC])->all();
+    $form->redesSociaisID = RedesSociais::find()->orderBy(['rede_social'=>SORT_ASC])->all();
+    $form->tagsID = Tags::find()->orderBy(['tag'=>SORT_ASC])->all();
+    
     if ( Yii::$app->request->isPost && $form->load(Yii::$app->request->post())  ) {
       $request = Yii::$app->request;
-      // $headers is an object of yii\web\HeaderCollection
-      //$headers = $request->headers;
-      //$tagsIds = $request->post('AnunciosTags');
-      //$tagsIds = $tagsIds['tag_id'];
-      //echo '<pre>'; print_r( $form ) ; echo '</pre>';die;
+      //echo '<pre>'; print_r($request->post()); echo '</pre>';die;
+      
+      /* Carrega todos atributos $_POST['CadastroAnuncioForm']
+      e faz um parsing para object já que o construtor espera um objeto*/
+      $anuncioForm = (object) $request->post('CadastroAnuncioForm');
+      /* Antes de destruir as propriedades que não fazem parte do
+      Objeto Anuncios, aloco em uma variavel local para depois sim
+      salvar no model AnunciosTags relação NxN entre Anuncios e Tags */
+      $tags_id = (isset($anuncioForm->tags_id)) ? $anuncioForm->tags_id : [];
+      // Distroi as propriedades que não existe no model Anuncios
+      unset($anuncioForm->cidade_id, $anuncioForm->tags_id);
 
       $transaction = Yii::$app->db->beginTransaction();
 
       try {
-        $model = new Anuncios();
-        $model->titulo = $form->titulo;
-        $model->slogan = $form->slogan;
-        $model->texto = $form->texto;
-        $model->telefone = $form->telefone;
-        $model->endereco = $form->endereco;
-        $model->site = $form->site;
-        $model->bairro_id = $form->bairro_id;
-        $model->categoria_id = $form->categoria_id;
-        $model->usuario_id = $form->usuario_id;
-
+        //echo '<pre>'; print_r($anuncioForm); echo '</pre>';die;
+        $model = new Anuncios($anuncioForm);
+        
         $model->save();
 
-        $tags = $request->post('AnunciosTags', array());
+        //$tags = $request->post('AnunciosTags', array());
 
-        foreach($tags as $tag_id => $arrai){
-
-          foreach($arrai as $tag){
-            $anuncTags = new AnunciosTags();
-            $anuncTags->anuncio_id = $model->id;
-            $anuncTags->tag_id = $tag;
-            ( !$anuncTags->save() ? $transaction->rollBack() : '' );
-          }
-          
+        foreach($tags_id as $tag_id){
+          $anuncTags = new AnunciosTags();
+          $anuncTags->anuncio_id = $model->id;
+          $anuncTags->tag_id = $tag_id;
+          ( !$anuncTags->save() ? $transaction->rollBack() : '' );
         }
 
         $redesSociais = $request->post('AnunciosRedesSociais', array());
@@ -209,25 +220,6 @@ class AnunciosController extends Controller
 
         }
 
-        if($form->imagens)
-        {
-          $uploadForm = new UploadForm();
-          $uploadForm->imagens = UploadedFile::getInstances($form, 'imagens');
-          $imagens = $uploadForm->multipleUpload();
-          
-          foreach ($imagens as $imagem) {
-            $upload = new Upload();
-            $upload->anuncio->id = $model->id;
-            $upload->diretorio = $imagem;
-            
-            ( !$upload->save() ? $transaction->rollBack() : '' );
-          }
-          
-        }
-        /*echo '<pre>';
-          var_dump($anunciosRedesSociais);
-        echo '</pre>';
-        exit();*/
         $transaction->commit();
 
       }catch(Exception $e){
@@ -238,8 +230,6 @@ class AnunciosController extends Controller
       return $this->redirect([
         'view',
         'id' => $model->id,
-        'bairro_id' => $model->bairro_id,
-        'categoria_id' => $model->categoria_id
       ]);
 
     } else {
@@ -258,12 +248,110 @@ class AnunciosController extends Controller
    * @param integer $categoria_id
    * @return mixed
   */
-  public function actionUpdate($id, $bairro_id, $categoria_id)
+  public function actionUpdate($id)
   {
-    $model = $this->findModel($id, $bairro_id, $categoria_id);
+    //Recebe uma instancia especifica do objeto Anuncio
+    $anuncio = $this->findModel($id);
+    $model = new CadastroAnuncioForm();
+    // Seta os novos valores que veio do form no $anuncio
+    $model->id = $anuncio->id;
+    $model->titulo = $anuncio->titulo;
+    $model->slogan = $anuncio->slogan;
+    $model->texto = $anuncio->texto;
+    $model->telefone = $anuncio->telefone;
+    $model->site = $anuncio->site;
+    $model->endereco = $anuncio->endereco;
+    $model->status = $anuncio->status;
+    $model->bairro_id = $anuncio->bairro_id;
+    $model->categoria_id = $anuncio->categoria_id;
+    $model->usuario_id = $anuncio->usuario_id;
 
-    if ($model->load(Yii::$app->request->post()) && $model->save()) {
-      return $this->redirect(['view', 'id' => $model->id, 'bairro_id' => $model->bairro_id, 'categoria_id' => $model->categoria_id]);
+    // Recebe todas as Redes Sociais que já estão salvas para este anuncio
+    $model->redes_sociais = $anuncio->anunciosRedesSociais;
+    
+    /* Carrega todas as instancias dos objetos abaixo para
+    preencher os campos do formulário */
+    $model->cidadesID = Cidades::find()->orderBy(['cidade' => SORT_ASC])->all();
+    $model->categoriasID = Categorias::find()->orderBy(['categoria' => SORT_ASC])->all();
+    $model->redesSociaisID = RedesSociais::find()->orderBy(['rede_social' => SORT_ASC])->all();
+    $model->tagsID = Tags::find()->orderBy(['tag' => SORT_ASC])->all();
+
+    if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+      
+      $anuncio->id = $model->id;
+      $anuncio->titulo = $model->titulo;
+      $anuncio->slogan = $model->slogan;
+      $anuncio->texto = $model->texto;
+      $anuncio->telefone = $model->telefone;
+      $anuncio->site = $model->site;
+      $anuncio->endereco = $model->endereco;
+      $anuncio->status = $model->status;
+      $anuncio->bairro_id = $model->bairro_id;
+      $anuncio->categoria_id = $model->categoria_id;
+      $anuncio->usuario_id = $model->usuario_id;
+      // Discarta todos os dados que só eram necessário para montar o form
+      unset($model->cidade_id, $model->cidadesID,$model->categoriasID,$model->redesSociaisID,$model->tagsID);
+      // Inicia a transação caso algum objeto não seja salvo desfaz toda a transação
+      $transaction = Yii::$app->db->beginTransaction();
+
+      try{
+        /**
+         * Tenta salvar os dados do model Anuncios para depois salvar
+         * os dados das tabelas NxN note que se qualquer operação
+         * dentro desse bloco try{} falhar nada é salvo
+        */
+        $anuncio->save();
+        /**
+         * $tags->delete() retorna um booleano true se sucesso
+         * Deleta todas as tags salva no relacionamento NxN entre
+         * Anuncio e AnunciosTags
+        * */
+        foreach ($anuncio->anunciosTags as $tags) {
+          $tags->delete();
+        }
+        /**
+         * Retorna todas as tags setada em CadastroAnuncioForm em forma de
+         * array 
+         * */ 
+        $tags_id = $model->tags_id;
+        /**
+         * Salva as novas tags selecionada no form, isso depois de deletar
+         *  as tags do relacionamento NxN
+        * */ 
+        foreach($tags_id as $tag_id){
+          $anuncTags = new AnunciosTags();
+          $anuncTags->anuncio_id = $anuncio->id;
+          $anuncTags->tag_id = $tag_id;
+          ( !$anuncTags->save() ? $transaction->rollBack() : '' );
+        }
+        
+        foreach($model->redes_sociais as $rSocial){
+          $rSocial->delete();
+        }
+        
+        $redesSociais = Yii::$app->request->post('AnunciosRedesSociais');
+        
+        foreach ($redesSociais as $redeSocial) {
+          // Setando os atributos do model AnunciosRedesSociais
+          $anunciosRedesSociais = new AnunciosRedesSociais();
+          // Atributo anuncio_id pertence a tabela Anuncios relação N X N
+          $anunciosRedesSociais->anuncio_id = $anuncio->id;
+          $anunciosRedesSociais->rede_social_id = $redeSocial['id'];
+          $anunciosRedesSociais->url = $redeSocial['url'];
+
+          (!$anunciosRedesSociais->save()) ? $transaction->rollBack() : '';
+
+        }
+
+        $transaction->commit();
+
+      }catch(Exception $e){
+        $transaction->rollBack();
+        throw $e;
+      }
+
+      return $this->redirect(['view', 'id' => $model->id]);
+
     } else {
       return $this->render('update', [
         'model' => $model,
@@ -279,13 +367,13 @@ class AnunciosController extends Controller
    * @param integer $categoria_id
    * @return mixed
   */
-  public function actionDelete($id, $bairro_id, $categoria_id)
+  public function actionDelete($id)
   {
-    $this->findModel($id, $bairro_id, $categoria_id)->delete();
+    $this->findModel($id)->delete();
     return $this->redirect(['index']);
   }
 
-  public function actionPesquisar($tag_id=NULL, $categoria_id=Null, $cidade_id=NULL, $bairro_id=NULL){
+  public function actionPesquisar($tag_id=NULL, $categoria_id=NUll, $cidade_id=NULL, $bairro_id=NULL){
     
     $model = new Anuncios();
 
@@ -309,9 +397,9 @@ class AnunciosController extends Controller
    * @return Anuncios the loaded model
    * @throws NotFoundHttpException if the model cannot be found
   */
-  protected function findModel($id, $bairro_id, $categoria_id)
+  protected function findModel($id)
   {
-    if (($model = Anuncios::findOne(['id' => $id, 'bairro_id' => $bairro_id, 'categoria_id' => $categoria_id])) !== null) {
+    if (($model = Anuncios::findOne(['id' => $id])) !== null) {
       return $model;
     } else {
       throw new NotFoundHttpException('The requested page does not exist.');
