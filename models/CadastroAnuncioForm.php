24e49d382dcf77f198d097f1aa17c4d8c560610d
Esteves <esteves@edusites.com.br>
2018-02-06 19:53:40 -0200

    Adicionado novas propriedades como  para atualizar os registros, e  que armazena propriedades da relação entre model Anuncios e AnunciosRedesSociais declarado como ->redes_sociais = ->anunciosRedesSociais; necessário para carregar os registros já salvo no DB e criar tag selected no dropdown e criado função setSelectedTags() que retorna todas as Tags

--- models/CadastroAnuncioForm.php
+++ models/CadastroAnuncioForm.php
@@ -10,10 +10,12 @@ namespace app\models;
 
 use Yii;
 use yii\base\Model;
+use app\models\AnunciosTags;
 
 class CadastroAnuncioForm extends Model
 {
   //Campos já existentes na tabela anuncios
+  public $id;
   public $titulo;
   public $slogan;
   public $texto;
@@ -23,24 +25,27 @@ class CadastroAnuncioForm extends Model
   public $bairro_id;
   public $categoria_id;
   public $usuario_id;
-  /*Campos necessários para atribuir valores que estão em outras
-  tabelas e carregar dados para alguns campos*/
+  public $status;
+  // Necessário para update
   public $cidade_id;
+  public $redes_sociais;
+  public $tags_id;
+  /*Campos necessários para atribuir valores que estão em outras
+  tabelas e carregar dados para o form*/
+  public $cidadesID;
+  public $categoriasID;
   public $redesSociaisID;
   public $tagsID;
-  //public $created_at;
-  public $imagens;
-
 
   public function rules(){
     return [
-      [['titulo', 'texto', 'telefone', 'endereco', 'bairro_id', 'categoria_id', 'cidade_id', 'usuario_id'], 'required'],
-      [['titulo', 'slogan'], 'string', 'max'=>200],
-      [['texto'], 'string'],
+      [['titulo', 'texto', 'telefone', 'endereco', 'bairro_id', 'categoria_id', 'cidade_id', 'usuario_id', 'tags_id'], 'required'],
+      [['titulo'], 'string', 'max'=>70],
+      [['slogan'], 'string', 'max'=>60],
+      [['texto'], 'string', 'max'=>1100],
       [['site'], 'string', 'max'=>45],
       [['bairro_id', 'categoria_id', 'usuario_id'], 'integer'],
-      //['created_at', 'default', 'value' => date('Y-m-d H:i:s')],
-      [['imagens'], 'file', 'skipOnEmpty' => false, 'extensions' => 'png, jpg', 'maxFiles' => 3],
+      //[['imagens'], 'file', 'skipOnEmpty' => false, 'extensions' => 'png, jpg', 'maxFiles' => 3],
     ];
   }
 
@@ -56,6 +61,20 @@ class CadastroAnuncioForm extends Model
       'cidade_id' => 'Cidade',
       'bairro_id' => 'Bairro',
       'categoria_id' => 'Categoria',
+      'tags_id' => 'Selecione até 3 palavras chaves'
     ];
   }
+
+  //Função para setar as tags selecionadas do anuncio
+  public function setSelectedTags()
+  {
+    $an_tags = AnunciosTags::find()->joinWith(['tag'])->
+    andWhere(['anuncio_id' => $this->id])->orderBy('tags.tag ASC')->all();
+    
+    foreach($an_tags as $an_tag)
+    {
+      $this->tags_id[] = $an_tag->tag_id;
+    }
+  }
+
 }
